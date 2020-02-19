30a2739f53f53c11b5ad92d58da4890b451466c8
lheddleston <laura.heddleston@gmail.com>
2020-02-19 11:57:53 -0800

    Refactor App into class component + make VideoPlayer dynamic

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,10 @@
 import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => {
-  // console.`log(props.videos[0].id.videoId);
-  // console.log('props in VideoList', props.exampleVideoData[0].videoId);
+
   return (
     <div className="video-list">
 
-      {props.videos.map( (video, index) => <VideoListEntry key={index} video={video} />)}
+      {props.videos.map( (video, index) => <VideoListEntry key={index} video={video} id ={indexgit } />)}
 
     </div>
   );
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,22 +1,19 @@
-var VideoPlayer = (props) => {
-
-  console.log(props.videos[0].id.videoId);
+var VideoPlayer = ({video}) => {
 
   return (
     <div className="video-player">
-
       <div className="embed-responsive embed-responsive-16by9">
 
         <iframe
           className="embed-responsive-item"
-          src={`https://www.youtube.com/embed/${props.videos[0].id.videoId}` }
+          src={`https://www.youtube.com/embed/${video.id.videoId}` }
           allowFullScreen>
         </iframe>
 
       </div>
       <div className="video-player-details">
-        <h3>{props.videos[0].snippet.title}</h3>
-        <div>{props.videos[0].snippet.description}</div>
+        <h3>{video.snippet.title}</h3>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );
@@ -25,9 +22,9 @@ var VideoPlayer = (props) => {
 // PropTypes tell other developers what `props` a component expects
 
 // Warnings will be shown in the console when the defined rules are violated
-VideoPlayer.propTypes = {
-  video: PropTypes.object.isRequired
-};
+// VideoPlayer.propTypes = {
+//   video: PropTypes.object.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
