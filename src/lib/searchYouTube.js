7e8a665f81df2f43a7ce9204e21952e5689f91c6
lheddleston <laura.heddleston@gmail.com>
2020-02-19 17:00:58 -0800

    Add GET call to You Tube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({query, max = 5, key, callback}) => {
+  $.get('https://www.googleapis.com/youtube/v3/search?', {
+    part: 'snippet',
+    q: query,
+    maxResults: max,
+    key: key,
+    type: 'video',
+    videoEmbeddable: 'true',
+    videoCaption: 'any'
+  })
+    .done(({items}) =>{
+      if (callback) {
+        callback(items);
+      }
+    })
+    .fail(({responseJSON}) => {
+      responseJSON.error.errors.forEach((err) => console.log(err));
+    });
 };
-
 export default searchYouTube;
